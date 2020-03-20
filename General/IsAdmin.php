90f35e82165c429c32c7927f33e188c60e3bf04f
pop-hip <56161185+pop-hip@users.noreply.github.com>
2020-03-20 19:04:55 +0100

    header en cours

--- General/IsAdmin.php
+++ General/IsAdmin.php
@@ -1,22 +1,32 @@
-<form action="index.php" method="post"> <input  type="submit" name="id" value="Passer Admin" > <input  type="hidden" name="isAdmin" value="true"></form>
+<head>
+	<link  href="assets/css/index.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
 
-<form action="index.php" method="post"> <input  type="submit" name="id" value="Passer User" > <input  type="hidden" name="isAdmin" value="false"></form>
+</head>
 
-<?php if(isset($_POST['isAdmin'])){
+<body>
 
-	if ($_POST['isAdmin'] == "true") {
-		echo "Admin","<br>";
-		$isAdmin = true;
-	}
-	else{
-		echo "Not Admin","<br>";
-		$isAdmin = false;
-	}
-	
+	<form id="bouttonAdmin" action="index.php" method="post"> <input  type="submit" name="id" value="Passer Admin" > <input  type="hidden" name="isAdmin" value="true"></form>
 
-} 
-else{
-	echo "Not Admin","<br>";
-	$isAdmin = false;
+	<form id="bouttonVisiteur" action="index.php" method="post"> <input  type="submit" name="id" value="Passer User" > <input  type="hidden" name="isAdmin" value="false"></form>
+	<div class="indicateur">
+		<?php if(isset($_POST['isAdmin'])){
 
-}?>
+			if ($_POST['isAdmin'] == "true") {
+				echo "Admin","<br>";
+				$isAdmin = true;
+			}
+			else{
+				echo "Not Admin","<br>";
+				$isAdmin = false;
+			}
+
+
+		} 
+		else{
+			echo "Not Admin","<br>";
+			$isAdmin = false;
+
+		}?>
+	</div>
+</body>
