3f1d30e86dd290a6c32cb1c5bbfb022824f2f9c6
Yovador <yovador@gmail.com>
2020-03-23 13:12:00 +0100

    Comment et Likes sur pages

---
+++ Article/CreateCom.php
@@ -0,0 +1,56 @@
+<?php 
+
+	include "../General/GetOneEntry.php";
+	include "../General/SelectList.php";
+
+	if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+		$Submit = isset($_POST['Submit']) ? $_POST['Submit'] : '';
+
+		
+		if (!empty($Submit) && $Submit == 'Valider') {
+			if ($_POST['PseudoAuteur'] != "" && $_POST['EmailAuteur'] != "" && $_POST['TitrCom'] != "" && $_POST['LibCom'] != ""){
+				include "../General/connectionBD.php";
+				try {
+					
+					include "NumCommentAndDuplicateCheck.php";
+					
+					date_default_timezone_set('Europe/Paris');
+					$DtCreC = date('Y-m-j H:i:s');
+					
+
+					if ($CanSend) {
+						$DB->beginTransaction();
+
+						$insert = $DB->prepare("INSERT INTO COMMENT (NumCom, DtCreC, PseudoAuteur, EmailAuteur, TitrCom ,LibCom, NumArt) VALUES(:NumCom, :DtCreC, :PseudoAuteur, :EmailAuteur, :TitrCom, :LibCom, :NumArt);");
+
+						$data = array(
+							':NumCom'=> $NumCom,
+							':DtCreC'=> $DtCreC,
+							':PseudoAuteur'=> $_POST["PseudoAuteur"],
+							':EmailAuteur'=> $_POST['EmailAuteur'],
+							':TitrCom'=> $_POST['TitrCom'],
+							':LibCom'=> $_POST['LibCom'],
+							':NumArt'=> $NumArt,
+						);
+
+						$insert->execute($data);
+						$DB->commit();
+					}
+					else{
+					}
+				
+				} 
+
+				catch (PDOException $e) {
+					echo $e;
+					$DB->rollBack();
+				}
+			}
+			else {
+			}
+		}
+
+	}
+
+?>
